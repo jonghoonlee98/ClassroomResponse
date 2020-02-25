cbdd40219a19a974456af3ca804ca019e782478a
Jong Hoon Lee <jonghoonlee98@gmail.com>
2020-02-25 17:04:29 -0500

    removing redudant files

--- django_school/classroom/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(User)
-admin.site.register(Course)
-admin.site.register(Quiz)
-admin.site.register(Student)
