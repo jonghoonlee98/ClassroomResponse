df718867195a3b90b3abcf14a71aef271c84f5c6
Jong Hoon Lee <jonghoonlee98@gmail.com>
2020-03-03 13:43:38 -0500

    local env to use sqlite

--- classroom_response/classroom_response/settings.py
+++ classroom_response/classroom_response/settings.py
@@ -94,12 +94,8 @@ if PROD != False:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'classroom',
-            'USER': 'admin',
-            'PASSWORD': 'classroom',
-            'HOST': 'localhost',
-            'PORT': '',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
