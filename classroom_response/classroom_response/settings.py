664b8dfb37c016959509712f34264afb71f25e15
Jong Hoon Lee <jonghoonlee98@gmail.com>
2020-03-03 19:22:15 -0500

    adding whitenoise

--- classroom_response/classroom_response/settings.py
+++ classroom_response/classroom_response/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'classroom_response.urls'
