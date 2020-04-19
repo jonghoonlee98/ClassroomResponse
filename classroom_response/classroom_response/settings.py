f53bbd6fb248cc0bdbfd7c4b8d4641cab385ae8c
Sang Woo Kim <sangk@spigen.com>
2020-04-19 03:10:21 -0700

    delete button

--- classroom_response/classroom_response/settings.py
+++ classroom_response/classroom_response/settings.py
@@ -35,6 +35,20 @@ if PROD == False:
 ALLOWED_HOSTS = ['localhost', 'classroomresponsesystem.herokuapp.com']
 
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
 # Application definition
 
 INSTALLED_APPS = [
